dd6b9ff359166972439913746bfffbed25da493f
nddmanh <manhnd.ilec@gmail.com>
2023-10-17 08:17:37 +0700

    folder setup

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <p className='text-3xl font-medium text-sky-700'>
+        Hello world
+      </p>
+      <Button variant="destructive">
+        CLick me
+      </Button>
+    </div>
+    
+  )
+}
