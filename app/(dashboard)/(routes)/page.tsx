407827adc7519b061507e80d17e682a271f61964
nddmanh <manhnd.ilec@gmail.com>
2023-10-18 07:54:25 +0700

    Authentication

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,15 +1,10 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+import { UserButton } from '@clerk/nextjs'
 
 export default function Home() {
   return (
     <div>
-      <p className='text-3xl font-medium text-sky-700'>
-        Hello world
-      </p>
-      <Button variant="destructive">
-        CLick me
-      </Button>
+      <UserButton afterSignOutUrl='/'>
+      </UserButton>
     </div>
     
   )
